2590a52112a8fff79ddd60b6980591111370700f
Vegely <bogurad.baranski@alumnos.upm.es>
2024-10-01 15:45:58 +0200

    5,018 segundos T_T

--- Projectos STM/ECC/Core/Inc/ecdh.h
+++ Projectos STM/ECC/Core/Inc/ecdh.h
@@ -60,7 +60,7 @@ extern "C" {
 
 /* What is the default curve to use? */
 #ifndef ECC_CURVE
- #define ECC_CURVE NIST_B163
+ #define ECC_CURVE NIST_K163
 #endif
 
 #if defined(ECC_CURVE) && (ECC_CURVE != 0)
@@ -84,7 +84,7 @@ extern "C" {
  #error Must define a curve to use
 #endif
 
-#define ECC_PUB_KEY_SIZE     (2 * )
+#define ECC_PUB_KEY_SIZE     (2 * ECC_PRV_KEY_SIZE)
 
 
 /****************************************ECC_PRV_KEY_SIZE**************************************/
@@ -95,11 +95,6 @@ int ecdh_generate_keys(uint8_t* public_key, uint8_t* private_key);
 
 /* input: own private key + other party's public key, output: shared secret */
 int ecdh_shared_secret(const uint8_t* private_key, const uint8_t* others_pub, uint8_t* output);
-
-
-/* Broken :( .... */
-int ecdsa_sign(const uint8_t* private_key, uint8_t* hash, uint8_t* random_k, uint8_t* signature);
-int ecdsa_verify(const uint8_t* public_key, uint8_t* hash, const uint8_t* signature);
 
 
 /******************************************************************************/
---
+++ Projectos STM/ECC/Debug/Core/Src/ecdh.d
@@ -0,0 +1,2 @@
+Core/Src/ecdh.o: ../Core/Src/ecdh.c ../Core/Inc/ecdh.h
+../Core/Inc/ecdh.h:
