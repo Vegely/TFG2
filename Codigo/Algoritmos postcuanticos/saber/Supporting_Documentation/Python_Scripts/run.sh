9465a39d0ecdd1da61f20460f0c4a039957f13af
Vegely <79764272+Vegely@users.noreply.github.com>
2025-05-05 03:34:43 +0200

    asd

--- Codigo/Algoritmos postcuanticos/saber/Supporting_Documentation/Python_Scripts/run.sh
+++
@@ -1,7 +0,0 @@
-# clone the estimator and setup in framework folder
-git clone https://github.com/lducas/leaky-LWE-Estimator.git tmp
-cp -r tmp/framework framework
-
-# run the script
-cd select_params
-sage select_params.py
