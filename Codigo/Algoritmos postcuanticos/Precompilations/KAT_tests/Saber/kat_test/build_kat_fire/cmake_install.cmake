8068ab96be572df7810778eefb061b9eef94895c
Vegely <79764272+Vegely@users.noreply.github.com>
2025-06-20 18:45:21 +0200

    dfda

--- Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/cmake_install.cmake
+++ Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/bogurad/Desktop/Proyectos gitHub/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test
+# Install script for directory: C:/Users/bogur/Desktop/Proyectos github/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/bogurad/Desktop/Proyectos gitHub/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/install_local_manifest.txt"
+  file(WRITE "C:/Users/bogur/Desktop/Proyectos github/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/bogurad/Desktop/Proyectos gitHub/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/bogur/Desktop/Proyectos github/TFG2/Codigo/Algoritmos postcuanticos/Precompilations/KAT_tests/Saber/kat_test/build_kat_fire/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
