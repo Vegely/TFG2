dc2208fd59a614d90d6cd4c782c27b167543fe04
Vegely <79764272+Vegely@users.noreply.github.com>
2025-10-13 20:17:05 +0200

    UART

--- Codigo/PSOC/PSoC 6 serial/SerialComunication.cydsn/main_cm4.c
+++ Codigo/PSOC/PSoC 6 serial/SerialComunication.cydsn/main_cm4.c
@@ -15,6 +15,24 @@
 #include "task.h"
 #include "timers.h"
 #include <stdio.h>
+#include "uartTask.h"
+
+
+
+int main(void)
+{
+    __enable_irq(); /* Enable global interrupts. */
+
+    /* Create RTOS tasks and start the scheduler */
+    xTaskCreate ( uartTask, "UART Task", 400, 0, 2, 0);
+    vTaskStartScheduler(); // This function never returns
+    
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
 /*void LED_TASK(void){
     for(;;)
     {
@@ -27,22 +45,22 @@
 //xTaskCreate(LED_TASK,"LED_TAST",400,NULL,1,0);
 //vTaskStartScheduler();
 
-int main(void)
-{
-    __enable_irq(); /* Enable global interrupts. */
+//int main(void)
+//{
+//    __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    UART_Start();
-    setvbuf(stdin,NULL,_IONBF,0); //No buffer
-    char c;
-    printf("Started UART\r\n");
-    for(;;)
-    {
-        c=getchar();
-        if(c){
-            printf("%c", c);
-        }
-    }
-}
+//    UART_Start();
+//    setvbuf(stdin,NULL,_IONBF,0); //No buffer
+//    char c;
+//    printf("Started UART\r\n");
+//    for(;;)
+//    {
+//        c=getchar();
+//        if(c){
+//            printf("%c", c);
+//        }
+//    }
+//}
 
 /* [] END OF FILE */
