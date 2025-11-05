4048f101ca0d23c618599665f3bb4dd47119e202
Vegely <79764272+Vegely@users.noreply.github.com>
2025-11-05 11:45:12 +0100

    Changed structure

---
+++ Codigo/Algoritmos postcuanticos/PSOC/Workspace/SABER/Saber/Serial/uartTask.h
@@ -0,0 +1,20 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#pragma once
+
+#include "semphr.h"
+
+void uartTask(void *arg);
+
+// Semaphore that signals when UART is initialized and ready
+extern SemaphoreHandle_t uartReadySemaphore;
+/* [] END OF FILE */
